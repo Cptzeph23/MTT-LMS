7e4861a142d03dc2a4ca7a8f46f3c8455fee7619
Cptzeph23 <asekazephaniah@gmail.com>
2026-01-22 14:59:32 +0300

    full prototype done

--- lms_portal/core/urls.py
+++ lms_portal/core/urls.py
@@ -1,11 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-
-
-
 urlpatterns = [
 
+
+    path('admin/', admin.site.urls),
     # AUTH
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
